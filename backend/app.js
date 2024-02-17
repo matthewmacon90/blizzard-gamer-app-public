c805873384668375f9d22c0856569ad1d5eef5f2
Matthew Macon <matthewmacon90@gmail.com>
2024-02-16 20:24:19 -0500

    setting up google OAuth

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,26 @@
 const express = require('express');
+const passport = require('passport');
+const session = require('express-session');
 const cors = require('cors');
 const morgan = require('morgan');
 
+//Routes
+const homeRoutes = require('./routes/home.js');
 const userRouter = require('./routes/users.js');
+const authenticateGoogleRoutes = require('./authentication/oauth2-google/authenticateGoogleRoutes.js');
+
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(morgan('tiny'));
+app.use(session({ secret: 'YOUR_SESSION_SECRET', resave: false, saveUninitialized: false, cookie: { secure: true}}));
+app.use(passport.initialize());
+app.use(passport.session());
 
+app.use('/', homeRoutes);
+app.use('/google', authenticateGoogleRoutes);
 app.use('/users', userRouter);
 
 app.use((err, req, res, next) => {
---
+++ backend/authentication/oauth2-google/authenticateGoogleRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const passport = require('passport');
+const router = express.Router();
+
+router.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+router.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/login' }), (req, res) => res.redirect('/'));
+
+// Logout route
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
+
+module.exports = router;
