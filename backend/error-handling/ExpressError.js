e8192a2f6d58de14ca96dd5f211893d34da6b6ed
Matthew Macon <matthewmacon90@gmail.com>
2024-02-18 18:06:30 -0500

    added json schema for new user

--- backend/error-handling/ExpressError.js
+++ backend/error-handling/ExpressError.js
@@ -6,4 +6,34 @@ class ExpressError extends Error {
   }
 }
 
-module.exports = ExpressError;
+class NotFoundError extends ExpressError {
+  constructor(message = "Not Found") {
+    super(message, 404);
+  }
+}
+
+class UnauthorizedError extends ExpressError {
+  constructor(message = "Unauthorized") {
+    super(message, 401);
+  }
+}
+
+class BadRequestError extends ExpressError {
+  constructor(message = "Bad Request") {
+    super(message, 400);
+  }
+}
+
+class ForbiddenError extends ExpressError {
+  constructor(message = "Bad Request") {
+    super(message, 403);
+  }
+}
+
+module.exports = {
+  ExpressError,
+  NotFoundError,
+  UnauthorizedError,
+  BadRequestError,
+  ForbiddenError,
+};
